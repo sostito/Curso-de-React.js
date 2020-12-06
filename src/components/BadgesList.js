9f6adca650e13d12cc5516398209128861452231
Freddy Rangel <freddymauran@gmail.com>
2020-12-05 19:00:47 -0500

    Se realiza consumo fake de tipo get para el listado de los badges

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import './styles/BadgesList.css';
 
 class BadgesListItem extends React.Component {
@@ -27,6 +27,16 @@ class BadgesListItem extends React.Component {
 
 class BadgesList extends React.Component {
   render() {
+    if(this.props.badges.lenght == 0) {
+      return (
+      <div>
+        <h3>No encontramos ningún badge</h3>
+        <Link className="btn btn-primary" to="/badges/new">
+          Create new badge
+        </Link>
+      </div>)
+    }
+
     return (
       <div className="BadgesList">
         <ul className="list-unstyled">
