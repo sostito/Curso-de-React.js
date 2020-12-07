2f732dfdc5200a7bc2cb468914415f602a588f62
Freddy Rangel <freddymauran@gmail.com>
2020-12-07 17:29:05 -0500

    Se implementa la actualizacion de datos

--- src/pages/BadgeEdit.js
+++ src/pages/BadgeEdit.js
@@ -1,19 +1,43 @@
 import React from 'react';
 
-import './styles/BadgeNew.css'
+import './styles/BadgeEdit.css'
 import header from '../images/platziconf-logo.svg'
 import Badge from '../components/Badge'
 import BadgeForm from '../components/BadgeForm'
 import api from '../api'
+import PageLoading from '../components/PageLoading'
+import PageError from '../components/PageError'
 
-class BadgeNew extends React.Component {
-  state = { form: {
-    firstName: '',
-    lastName: '',
-    email: '',
-    jobTitle: '',
-    twitter: ''
-  } }
+class BadgeEdit extends React.Component {
+  state = { 
+    loading: true,
+    error: null,
+    form: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      jobTitle: '',
+      twitter: ''
+    } 
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async e => {
+    this.setState({ loading: true, error: null });
+
+    try {
+      const data = await api.badges.read(
+        this.props.match.params.badgeId
+      )
+
+      this.setState({ loading: false, form: data });
+    } catch(error) {
+      this.setState({ loading: false, error: error });
+    }
+  }
 
   handleChange = e => {
     this.setState({
@@ -30,7 +54,7 @@ class BadgeNew extends React.Component {
     this.setState({ loading: true, error: null });
 
     try {
-      await api.badges.create(this.state.form);
+      await api.badges.update(this.props.match.params.badgeId, this.state.form);
       this.setState({ loading: false });
     } catch(error) {
       this.setState({ loading: false, error: error });
@@ -38,11 +62,20 @@ class BadgeNew extends React.Component {
   }
 
   render() {
+
+    if (this.state.loading == true) {
+      return <PageLoading />
+    }
+    
+    if (this.state.error) {
+      return <PageError error={this.state.error}/>
+    }
+
     return (
       <React.Fragment>
-        <div className="BadgeNew__hero">
+        <div className="BadgeEdit__hero">
           <img 
-              className="BadgeNew_hero-image img-fluid" 
+              className="BadgeEdit_hero-image img-fluid" 
               src={header} 
               alt="Logo">              
           </img>
@@ -61,7 +94,7 @@ class BadgeNew extends React.Component {
             </div>
 
             <div className="col-6">
-              <h1>New Attendant</h1>
+              <h1>Edit Attendant</h1>
               <BadgeForm 
                 otherEvent={this.handleChange}
                 onSubmit={this.handleSubmit}
@@ -74,4 +107,4 @@ class BadgeNew extends React.Component {
   }
 }
 
-export default BadgeNew;
+export default BadgeEdit;
