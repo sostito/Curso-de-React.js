6d4d31dcbb88030a62eaaef30acc5eae6027cfb2
Freddy Rangel <freddymauran@gmail.com>
2020-12-25 13:07:29 -0500

    Se separa logica de presentacion para la vista BadgeDetail

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import Home from '../pages/Home';
 import Badges from '../pages/Badges';
 import BadgeNew from '../pages/BadgeNew';
 import BadgeEdit from '../pages/BadgeEdit';
-import BadgeDetails from '../pages/BadgeDetails';
+import BadgeDetailsContainer from '../pages/BadgeDetailsContainer';
 import NotFound from '../pages/NotFound';
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
           <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
-          <Route exact path="/badges/:badgeId" component={BadgeDetails} />
+          <Route exact path="/badges/:badgeId" component={BadgeDetailsContainer} />
           <Route component={NotFound} />
         </Switch>
       </Layout>
--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,48 +1,13 @@
 import React, { Component } from 'react'
+
 import { Link } from 'react-router-dom';
-
-import './styles/BadgeDetails.css'
 import confLogo from '../images/platziconf-logo.svg';
-import PageLoading from '../components/PageLoading';
-import PageError from '../components/PageError';
 import Badge from '../components/Badge';
-import api from '../api'
 
-export class BadgeDetails extends Component {
-    state = {
-        loading: true,
-        error: null,
-        data: undefined
-    };
+function BadgeDetails(props) {
 
-    componentDidMount() {
-        this.fetchData()
-    }
-
-    fetchData = async () => {
-        this.setState({loading: true, error: null});
-
-        try {
-            const data = await api.badges.read(
-                this.props.match.params.badgeId
-            )
-            this.setState({loading: false, data: data});
-        } catch (error) {
-            this.setState({loading: false, error: error});
-        }
-    }
-    render() {
-        if(this.state.loading) {
-            return <PageLoading />
-        }
-
-        if(this.state.error) {
-            return <PageError error={this.state.error} />
-        }
-
-        const badge = this.state.data;
-
-        return (
+    const badge = props.badge;
+    return (
             <div>
                 <div className="BadgeDetails__hero">
                     <div className="container">
@@ -83,8 +48,7 @@ export class BadgeDetails extends Component {
                     </div>
                 </div>
             </div>
-        )
-    }
+    )
 }
 
 export default BadgeDetails
