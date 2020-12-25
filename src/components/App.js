8dd90e91a525d365e38195deb7418e95ee957f77
Freddy Rangel <freddymauran@gmail.com>
2020-12-25 12:57:48 -0500

    Se implementa vista para mostrar detalles de una persona

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Home from '../pages/Home';
 import Badges from '../pages/Badges';
 import BadgeNew from '../pages/BadgeNew';
 import BadgeEdit from '../pages/BadgeEdit';
+import BadgeDetails from '../pages/BadgeDetails';
 import NotFound from '../pages/NotFound';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
           <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
+          <Route exact path="/badges/:badgeId" component={BadgeDetails} />
           <Route component={NotFound} />
         </Switch>
       </Layout>
